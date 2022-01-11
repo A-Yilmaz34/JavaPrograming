59ac5b2009488d44ba6b820910e94552c1e924bf
A-Yilmaz34 <erolyz55@gmail.com>
2022-01-11 23:07:12 +0300

    initial Commit

---
+++ src/day02_HelloWorld/PrintExecution.java
@@ -0,0 +1,7 @@
+package day02_HelloWorld;
+
+public class PrintExecution {
+    public static void main(String[] args) {
+        System.out.println("Duygu\nOlexandr\nZoia\n\nAhmet\nYuliya\nCasandra\n\nCydeo");
+    }
+}
---
+++ src/practice/EscapeSequences.java
@@ -0,0 +1,15 @@
+
+package practice;
+
+public class EscapeSequences {
+    public static void main (String[] args){
+        System.out.println("Hello, How are you today? \nI am good");//satır atlama
+        System.out.println("\tJava is fun"); //paragraf başı
+        System.out.println("\\"); //backslash yazmak için 2 tane yapılır.
+        System.out.println("My favourite programming language is \"Java\""); // \işareti kensinden sonraki özel işaret de olsa yazdırır
+/*birden fazla satırda yorum yazmak için bu kullanılır.
+
+ */
+    }
+
+}
