882cd903eb12a08b711440b0d9aaca62b795f06b
A-Yilmaz34 <erolyz55@gmail.com>
2022-04-06 22:02:12 +0300

    codes

--- src/day25_CustomMethod_Overloading/MainMethodOverloading.java
+++ src/day25_CustomMethod_Overloading/MainMethodOverloading.java
@@ -2,11 +2,11 @@ package day25_CustomMethod_Overloading;
 
 public class MainMethodOverloading {
     public static void main(String[] args) {
-
+        System.out.println(12);
     }
 
     public static void main(int[] args) {
-
+        System.out.println(2);
     }
 
     public static void main(double[] args) {
--- src/day34_GarbageCollection_AccessModifiers/GarbageCollection.java
+++ src/day34_GarbageCollection_AccessModifiers/GarbageCollection.java
@@ -1,15 +1,17 @@
 package day34_GarbageCollection_AccessModifiers;
 
 import day30_CustomClassIntro.Dog;
+import day30_CustomClassIntro.Student;
 import day32_CustomClassConstructors.Car;
 import day33_Statics.Iphone;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GarbageCollection {
     public static void main(String[] args) {
 
-        String str = null;
+        String str = "";
 
         System.out.println(str.toUpperCase());
 
@@ -36,6 +38,28 @@ public class GarbageCollection {
         ArrayList<Integer> list1 = new ArrayList<>();
 
         list1.add(100);
+        ArrayList<Integer> list2 = list1;
+
+        list2.addAll(Arrays.asList(200,300,400));
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+        System.out.println(list1 == list2);
+
+        System.out.println("-----------------------------------");
+
+
+        ArrayList<String> l1 = new ArrayList<>();
+        l1.add("Java");
+
+        ArrayList<String> l2 = l1;
+        l2.add("Python");
+
+
+        System.out.println("l1 = " + l1);
+        System.out.println("l2 = " + l2);
+
 
 
     }
