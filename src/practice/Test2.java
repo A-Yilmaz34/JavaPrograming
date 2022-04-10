1ba4a51a649f8a0b4fdf6947a3f6b07dafffbf3f
A-Yilmaz34 <erolyz55@gmail.com>
2022-04-10 19:36:43 +0300

    codes

--- src/practice/Test2.java
+++ src/practice/Test2.java
@@ -7,13 +7,6 @@ public class Test2 {
 
     public static void main(String[] args) {
 
-        String str = "the fox ran under the bridge";
-
-        str = str.substring(4,17);
-
-        str.toUpperCase();
-
-        System.out.println(str);
 
 
 
